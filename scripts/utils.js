d68a987eb60e19e8bfd680bad29cce14eb146ef6
urchifox <126313096+urchifox@users.noreply.github.com>
2024-04-08 12:51:01 +0000

    Deploying to gh-pages from @ urchifox/test-to-lepekhin-studio@c41eaf37104f1a170fbfd25bd086184f8be46dc3 🚀

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,1 +1,1 @@
-var l=i=>i.scrollHeight>i.clientHeight;export{l as isOverflowedVertically};
+var t=i=>i.scrollHeight>i.clientHeight,e=i=>window.getComputedStyle(i).opacity==="1";export{t as isOverflowedVertically,e as isVisible};
