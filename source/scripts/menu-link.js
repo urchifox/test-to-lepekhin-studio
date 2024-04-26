d16f689680c73bd465484dcb9221a2fb8ca50db0
urchifox <126313096+urchifox@users.noreply.github.com>
2024-04-26 17:05:02 +0400

    add declensions

--- source/scripts/menu-link.js
+++ source/scripts/menu-link.js
@@ -1,16 +1,19 @@
+import getDeclension from '@dubaua/get-declension';
+
 const links = document.querySelectorAll('.menu__link[data-counter]');
 const projectLinks = document.querySelectorAll('.menu__link--type-project');
 
-const getTemplate = (count) => `
+const getTemplate = (count, word) => `
   <div class="menu__counter">
     <div class="menu__count-number">${count}</div>
-    <span class="visually-hidden">уведомлений</span>
+    <span class="visually-hidden">${word}</span>
   </div>
 `;
 
 const addCounter = (count, parentLink) => {
+  const word = getDeclension({ count: parseInt(count, 10), one: 'уведомление', few: 'уведомления', many: 'уведомлений' }).split(' ')[1];
   const temporaryStore = document.createElement('div');
-  temporaryStore.innerHTML = getTemplate(count);
+  temporaryStore.innerHTML = getTemplate(count, word);
   parentLink.appendChild(temporaryStore.firstElementChild);
 };
 
